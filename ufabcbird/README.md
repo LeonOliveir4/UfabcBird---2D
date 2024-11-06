1d635ebfc053639150c605fac0f0d08f1d247623
Leon <77010123+LeonOliveir4@users.noreply.github.com>
2024-11-05 21:01:56 -0300

    Adicionando States e Colisoes
    Adicionando States e Colisoes

---
+++ ufabcbird/README.md
@@ -0,0 +1,64 @@
+# Projeto UfabcBirds - Computação Gráfica
+
+**Disciplina:** Computação Gráfica  
+**Professor:** CELSO SETSUO KURASHIMA 
+**Período:** 2024.3
+
+---
+
+### Desenvolvedores
+
+- **Leonardo Pires de Oliveira** - 11201920744
+- **Leopoldo Kenji Sugata Naves** - 11201722022
+
+---
+
+## Descrição do Projeto
+
+Este projeto foi desenvolvido como parte das atividades práticas da disciplina de **Computação Gráfica**. O objetivo é criar uma versão personalizada do jogo **Flappy Bird**, com diversificação de espécies, e tendo como protagonista a fauna brasileira, utilizando a biblioteca **ABCg** e **OpenGL** para renderização gráfica.
+
+O jogo apresenta algumas diferenças em relação ao Flappy Bird clássico, como:
+
+- [Adicionar características diferenciadas]
+- [Outro diferencial ou mecânica de jogo]
+
+O jogo foi implementado em **C++** e segue uma estrutura modular para facilitar a expansão de funcionalidades e mecânicas de jogo.
+
+---
+
+## Aplicabilidade
+
+É um jogo com enfoque em entretenimento, mas que também trará o conhecimento a respeito da fauna brasileira na educação infantil. O jogo incentiva a curiosidade e o amor pela natureza, despertando o interesse em aprender mais sobre as espécies que habitam o Brasil.
+
+## Estrutura do Projeto
+
+O projeto está organizado da seguinte maneira:
+
+- **`main.cpp`**: Contém a função principal do jogo, que inicializa a janela e as configurações básicas do OpenGL.
+- **`window.hpp`**: Declara a classe `Window`, que gerencia a lógica do jogo e as renderizações.
+- **`window.cpp`**: Implementa os métodos da classe `Window`, responsáveis por controlar o loop de jogo, renderização dos objetos e interação do usuário.
+
+---
+
+## Tecnologias Utilizadas
+
+- **Linguagem**: C++
+- **Biblioteca Gráfica**: OpenGL
+- **Biblioteca de Suporte**: ABCg (biblioteca fornecida pelo professor)
+- **Plataforma de Desenvolvimento**: Linux
+- **IDE**: Visual Studio
+
+---
+
+## Como Executar o Projeto
+
+1. Clone este repositório no seu ambiente local:
+   ```bash
+   git clone [URL do repositório]
+
+## Considerações Finais
+
+Através do "UfabcBirds", esperamos proporcionar uma experiência divertida e educativa, contribuindo para a conscientização sobre a biodiversidade brasileira e a importância da preservação ambiental. Agradecemos ao professor Celso Setsuo Kurashima pelo suporte e orientação durante o desenvolvimento do projeto!
+
+
+
---
+++ ufabcbird/window.cpp
@@ -0,0 +1,119 @@
+#include "window.hpp"
+#include <iostream>
+
+void Window::onCreate() {
+    auto const assetsPath{abcg::Application::getAssetsPath()};
+    std::cout << "Initialize shaders \n";
+    std::cout << "estado do jogo: " << static_cast<char>(m_gameData.m_state);
+    m_birdProgram =
+      abcg::createOpenGLProgram({{.source = assetsPath + "bird.vert",
+                                  .stage = abcg::ShaderStage::Vertex},
+                                 {.source = assetsPath + "bird.frag",
+                                  .stage = abcg::ShaderStage::Fragment}});
+    m_bgProgram =
+      abcg::createOpenGLProgram({{.source = assetsPath + "bg.vert",
+                                  .stage = abcg::ShaderStage::Vertex},
+                                 {.source = assetsPath + "bg.frag",
+                                  .stage = abcg::ShaderStage::Fragment}});
+    std::cout << "Shader ok \n";
+    abcg::glClearColor(0.5f, 0.5f, 0.5f, 1);
+#if !defined(__EMSCRIPTEN__)
+  abcg::glEnable(GL_PROGRAM_POINT_SIZE);
+#endif
+    restart();
+}
+
+void Window::restart() {
+    m_gameData.m_state = State::Playing;
+    std::cout << "estado do jogo restart: " << static_cast<int>(m_gameData.m_state) << "\n";
+    m_bird.create(m_birdProgram, m_gameData);
+    m_bg.create(m_bgProgram, m_gameData);
+}
+
+void Window::onEvent(SDL_Event const &event) {
+  // Keyboard events
+  if (event.type == SDL_KEYDOWN) {
+    if (event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_w)
+      m_gameData.m_input.set(gsl::narrow<size_t>(Input::Up));
+  }
+  if (event.type == SDL_KEYUP) {
+    if (event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_w)
+      m_gameData.m_input.reset(gsl::narrow<size_t>(Input::Up));
+  }
+}
+
+void Window::onUpdate() {
+  if (m_gameData.m_state != State::Playing &&
+      m_restartTimer.elapsed() > 3) {
+    restart();
+    return;
+  }
+
+  auto const deltaTime{gsl::narrow_cast<float>(getDeltaTime())};
+  m_bg.update(m_gameData, deltaTime);
+  m_bird.update(m_gameData, deltaTime);
+
+  if (m_gameData.m_state == State::Playing) {
+    checkCollisions();
+  }
+}
+
+void Window::onPaint() {
+  abcg::glClear(GL_COLOR_BUFFER_BIT);
+  abcg::glViewport(0, 0, m_viewportSize.x, m_viewportSize.y);
+  m_bg.paint();
+  m_bird.paint(State::Playing);
+  
+}
+
+void Window::onPaintUI() {
+    abcg::OpenGLWindow::onPaintUI();
+    ImU32 redColor = IM_COL32(255, 0, 0, 125); // RGBA
+    // Desenha um retângulo que cobre toda a janela
+    ImDrawList* draw_list = ImGui::GetBackgroundDrawList();
+    draw_list->AddRectFilled(ImVec2(0, 0), ImVec2(m_viewportSize.x, 50), redColor);
+    {
+        ImGui::SetNextWindowPos(ImVec2(0,0));
+        ImGui::SetNextWindowSize(ImVec2(m_viewportSize.x,50));
+        ImGuiWindowFlags const flags{ImGuiWindowFlags_NoBackground |
+                                    ImGuiWindowFlags_NoTitleBar };//| ImGuiWindowFlags_NoInputs};
+        ImGui::Begin(" ", nullptr, flags);
+        ImGui::PushItemWidth(50);
+        ImGui::SetCursorPos(ImVec2(0, 0));
+        ImGui::PushItemWidth(100);
+        ImGui::SliderFloat("Sustein", &m_bird.m_sustein.y, -10.0f, 10.0f);
+        ImGui::SetCursorPos(ImVec2(100, 0));
+        ImGui::SliderFloat("Flap time A", &m_bird.m_flapTimeA, 0.0f, 3.0f);
+        ImGui::SetCursorPos(ImVec2(300, 0));
+        ImGui::SliderFloat("Flap time B", &m_bird.m_flapTimeB, 0.0f, 3.0f);
+        ImGui::SetCursorPos(ImVec2(400, 0));
+        ImGui::SliderFloat("Flap power", &m_bird.m_flapPower.y, 0.0f, 100.0f);    
+        ImGui::SetCursorPos(ImVec2(0, 10));
+                ImGui::PushItemWidth(200);
+        ImGui::SliderFloat("Scale", &m_bird.m_scale, 0.0f, 5.0f);  
+        ImGui::PopItemWidth();
+
+        if (m_gameData.m_state == State::GameOver) {
+          ImGui::Text("Game Over!");
+        }
+
+        ImGui::End();
+    }
+}
+
+void Window::onResize(glm::ivec2 const &size) {
+  m_viewportSize = size;
+
+  abcg::glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Window::onDestroy() {
+  abcg::glDeleteProgram(m_birdProgram);
+  abcg::glDeleteProgram(m_bgProgram);
+  m_bg.destroy(); 
+  m_bird.destroy();
+}
+
+void Window::checkCollisions() {
+  return;
+}
